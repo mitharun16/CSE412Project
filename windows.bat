47d621326c3ca122d028892419b832a6836c7911
Aash Appakonam <aasha2winsa@gmail.com>
2022-11-30 22:29:28 -0700

    Updates bat file for windows, webpage auto open now

--- windows.bat
+++ windows.bat
@@ -4,6 +4,6 @@ createuser --superuser postgres
 psql -d postgres -a -f CreateDatabase.sql
 call npm init -y
 call npm i express pg cors
-call node index.js
 call start http://localhost:3000/home.html
+call node index.js
 @REM call pg_ctl -D Database stop
